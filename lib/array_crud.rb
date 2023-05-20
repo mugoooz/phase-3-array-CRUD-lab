a3a46b73050d0110b158e1cec77e7ec07dc3ce08
Eric Mugo <mugoericpg20@gmail.com>
2023-05-20 06:42:21 +0300

    Added elements to arrays

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,11 +9,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+    array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
